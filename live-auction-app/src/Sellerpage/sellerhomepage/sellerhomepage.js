fa5650385926b5845db6081da4635c38378051f5
Ramses C. Labora <ramzaa941@gmail.com>
2024-10-20 00:03:55 +1100

    [Vid][Ram]
    Added some buttons

--- live-auction-app/src/Sellerpage/sellerhomepage/sellerhomepage.js
+++ live-auction-app/src/Sellerpage/sellerhomepage/sellerhomepage.js
@@ -1,15 +1,48 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './sellerhomepage.css';
+import { FaVideo } from "react-icons/fa";
+import { SiGoogleanalytics } from "react-icons/si";
+import { FaShoppingCart } from "react-icons/fa";
+import { IoMdAdd } from "react-icons/io";
 
 function SellerHomePage() {
   const navigate = useNavigate();
   const [isNavOpen, setIsNavOpen] = useState(false);
   const [showLogoutModal, setShowLogoutModal] = useState(false);
 
+  const [stream, setStream] = useState(null);
+
+
+
+{stream && (
+  <div className="webcam-container">
+    <video
+      ref={(videoElement) => {
+        if (videoElement) {
+          videoElement.srcObject = stream;
+        }
+      }}
+      autoPlay
+      playsInline
+    />
+  </div>
+)}
+
+
   const toggleNav = () => {
     setIsNavOpen(!isNavOpen);
   };
+
+  const handleGoLive = async () => {
+    try {
+      const mediaStream = await navigator.mediaDevices.getUserMedia({ video: true });
+      setStream(mediaStream);
+    } catch (error) {
+      console.error('Error accessing webcam:', error);
+    }
+  };
+  
 
   const handleAddNewItem = () => {
     navigate('/addnewitem');
@@ -49,10 +82,18 @@ function SellerHomePage() {
           <span></span>
         </div>
         <nav className={`side-nav ${isNavOpen ? 'open' : ''}`}>
-          <button className="dashboard-button" onClick={handleAddNewItem}>Add new Item</button>
-          <button className="dashboard-button" onClick={handleViewItems}>View My Items</button>
-          <button className="dashboard-button" onClick={handleViewSales}>View Sales Analytics</button>
+          <button className="dashboard-button" onClick={handleAddNewItem}><IoMdAdd />Add new Item</button>
+          <button className="dashboard-button" onClick={handleViewItems}>
+          <FaShoppingCart /> View My Items
+</button>
+
+          <button className="dashboard-button" onClick={handleViewSales}>
+  <SiGoogleanalytics /> Analytics
+</button>
           <button className="dashboard-button logout" onClick={handleLogoutClick}>Logout</button>
+          <button className="dashboard-button" onClick={handleGoLive}>
+    <FaVideo /> Go Live
+  </button>
         </nav>
       </div>
 
